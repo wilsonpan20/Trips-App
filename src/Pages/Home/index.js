9339d2026b537201ff797322928eb5820d8c668d
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-23 10:19:40 -0300

    configurado api

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import api from '../../services/api'
 
 export default function Home(){
+  const [trips,setTrips] = useState([]);
+
+  useEffect(()=>{
+      async function loadApi(){
+          const response = await api.get('trips')
+          console.log(response.data)
+          setTrips(response.data)
+      }
+
+      loadApi()
+  },[])
+
     return(
         <div>
             <h1>Home</h1>
