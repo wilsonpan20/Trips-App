8fefff9e2ba35296e1830972c091414416f302d8
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-23 10:42:28 -0300

    Finalizado a home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,7 @@
 import React,{useState,useEffect} from "react";
 import api from '../../services/api'
+import {MdOutlineFlightTakeoff} from 'react-icons/md'
+import './style.css'
 
 export default function Home(){
   const [trips,setTrips] = useState([]);
@@ -7,7 +9,6 @@ export default function Home(){
   useEffect(()=>{
       async function loadApi(){
           const response = await api.get('trips')
-          console.log(response.data)
           setTrips(response.data)
       }
 
@@ -15,8 +16,24 @@ export default function Home(){
   },[])
 
     return(
-        <div>
-            <h1>Home</h1>
+        <div className="box">
+            {trips.map(trip=>(
+                <li key={trip.id}>
+                    <img src={trip.image} alt={trip.title} />
+                    <strong>{trip.title}</strong>
+                    <span> Staus: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
+                    <button
+                    type="button"
+                    onClick={()=>{}}
+                    >
+                        <div>
+                            <MdOutlineFlightTakeoff size={16} color="#fff"/>
+                        </div>
+                    <span>SOLICITAR RESERVA</span>        
+                    </button>
+
+                </li>
+            ))}
         </div>
     )
 }
