05b2cf8860481b2f5f43d6c184533ea0179b4d54
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-23 11:19:30 -0300

    Melhorando o arquivo de rotas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from "react";
-import Routes from "./routes";
-import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import Routes from './routes';
+import Header from './components/Header';
 
-export default function App (){
-  return(
- 
-      <BrowserRouter>
-      <Header/>
+export default function App() {
+ return (
+   <BrowserRouter>
+     <Header/>
      <Routes/>
-     </BrowserRouter>
-    
-  )
-
+   </BrowserRouter>
+ );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Logo from '../../assets/logo.svg'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
 import './style.css'
 
-export default function Header(){
-    return(
-        <header className="container">
-         <Link  to="/">
-         <img className="logo" src={Logo} alt="Logo Projeto" />
-         </Link>
-         <Link  className="reserva" to="/reservas">
-             <div>
-                 <strong>Minhas reservas</strong>
-                 <span>3 reserva</span>
-             </div>
-         </Link>
-        </header>
-    )
+export default function Header() {
+ return (
+   <header className="container">
+
+     <Link to="/">
+      <img className="logo" src={logo} alt="Logo Projeto" />
+     </Link>
+
+     <Link  className="reserva" to="/reservas">
+       <div>
+         <strong>Minhas reservas</strong>
+         <span>3 reservas</span>
+       </div>
+     </Link>
+
+   </header>
+ );
 }
