ad8c51d764c7b168d2c9c7fe5dc8099b9f8ef1a3
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-23 09:44:11 -0300

    Criando component de Header e stilizando

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from '../../assets/logo.svg'
+import './style.css'
 
 export default function Header(){
     return(
-        <header>
-            <h1>Header...</h1>
+        <header className="container">
+         <Link  to="/">
+         <img className="logo" src={Logo} alt="Logo Projeto" />
+         </Link>
+         <Link className="reserva" to="/reserva">
+             <div>
+                 <strong>Minhas reservas</strong>
+                 <span>3 reserva</span>
+             </div>
+         </Link>
         </header>
     )
 }
