f5834539eaa72b43e91a4f18971a3f563e3f95bc
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-25 05:51:52 -0300

    Criando e validado o estoque

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,5 +1,5 @@
 import { select, call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess,updateAmountReserve } from './actions';
+import { addReserveSuccess,updateAmountSucccess } from './actions';
 import api from '../../../services/api';
 
 function* addToReserve({ id }){
@@ -9,9 +9,23 @@ function* addToReserve({ id }){
 
   );
 
+ const myStock = yield call(api.get,`/stock/${id}`);
+
+  const stockAmount = myStock.data.amount;
+
+  const currentStock = tripExists ? tripExists.amount : 0;
+  
+  const amount = currentStock + 1;
+
+  if(amount > stockAmount){
+    alert('Quantidade maxima atingida');
+    return;
+  }
+
+
   if(tripExists){
-  const amount = tripExists.amount + 1;
-  yield put(updateAmountReserve(id,amount));
+  
+  yield put(updateAmountSucccess(id,amount));
 
   }else{
     const response = yield call(api.get, `trips/${id}` );
@@ -24,6 +38,22 @@ function* addToReserve({ id }){
   }
 };
 
+function* updateAmount({id,amount}){
+  if(amount <= 0) return;
+
+  const myStock = yield call(api.get,`/stock/${id}`);
+
+  const stockAmount = myStock.data.amount;
+
+  if(amount > stockAmount ){
+    alert('Quantidade maxima atingida');
+    return;
+  }
+   yield put(updateAmountSucccess(id,amount));
+}
+
 export default all([
-  takeLatest('ADD_RESERVE_REQUEST', addToReserve)
+  takeLatest('ADD_RESERVE_REQUEST', addToReserve),
+  takeLatest('UPDATE_RESERVE_REQUEST', updateAmount),
+
 ])
