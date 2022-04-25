02b95086a1ae5a047508a5137fe4c22737288191
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-24 21:45:38 -0300

    Melhorando o codigo com redux saga

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,12 +1,28 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess } from './actions';
+import { select, call, put, all, takeLatest } from 'redux-saga/effects';
+import { addReserveSuccess,updateAmountReserve } from './actions';
 import api from '../../../services/api';
 
 function* addToReserve({ id }){
-    const response = yield call(api.get, `trips/${id}` );
 
-    yield put(addReserveSuccess(response.data))
-}
+  const tripExists = yield select(
+   state => state.reserve.find(trip => trip.id === id)
+
+  );
+
+  if(tripExists){
+  const amount = tripExists.amount + 1;
+  yield put(updateAmountReserve(id,amount));
+
+  }else{
+    const response = yield call(api.get, `trips/${id}` );
+    const data = {
+      ...response.data,
+      amount:1,
+    };
+  
+    yield put(addReserveSuccess(data))
+  }
+};
 
 export default all([
   takeLatest('ADD_RESERVE_REQUEST', addToReserve)
